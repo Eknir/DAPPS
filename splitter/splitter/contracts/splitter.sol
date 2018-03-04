2cbabf952d69145d82539742c5152cb129ff5aea
Rinke Hendriksen <33000441+Eknir@users.noreply.github.com>
2018-03-04 23:14:07 +0100

    Add files via upload

--- splitter/splitter/contracts/splitter.sol
+++ splitter/splitter/contracts/splitter.sol
@@ -1,12 +1,14 @@
 pragma solidity ^0.4.17;
 
 contract Splitter {
-	address Alice;
-	address Bob;
-	Address Alice;
+	address alice;
+	address bob;
+	address carol;
 
-	function Splitter() public payable {
-		owner = msg.sender;
+	function Splitter(address _bob, address _carol) public payable {
+		alice = msg.sender;
+		bob = _bob;
+		carol = _carol;
 	}
 
 	function getBalance() public view returns (uint) {
@@ -14,12 +16,25 @@ contract Splitter {
 	}
 
 	function splitAlice() public payable returns (bool) {
-		assert(msg.sender == Alice)
-		//TODO
+		require(msg.sender == alice);
+		uint value = msg.value;
+		uint half = value /2;
+		bob.transfer(half);
+		carol.transfer(half);
+		return true;
 	}
 
 	function getIndividualBalance() public view returns (
 		uint,
 		uint,
-		uint)
+		uint) {
+
+		return(alice.balance, bob.balance, carol.balance);
+		    
+		}
+
+
+	function() public payable {
+
+	}
 }
