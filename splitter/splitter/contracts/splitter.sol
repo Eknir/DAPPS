857908fc61699fcb77fa3fac179eaa03221a038c
Rinke Hendriksen <33000441+Eknir@users.noreply.github.com>
2018-03-06 18:43:12 +0100

    Add files via upload

--- splitter/splitter/contracts/splitter.sol
+++ splitter/splitter/contracts/splitter.sol
@@ -2,60 +2,54 @@ pragma solidity ^0.4.17;
 
 contract Splitter {
 	address alice;
-	address bob// = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c; // fill in yourself!
-	address carol// = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db; // fill in yourself!
+	address bob; //= 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c; 
+	address carol; // =0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
 	
-	event logDeposit(uint _depositAmount);
-	event logWithdrawal(address _receiver, uint _value);
-	event logSplit(uint _splitAmount);
+	event LogDeposit(uint _depositAmount);
+	event LogSplit(uint _newBalanceBob, uint _newBalanceCarol);
+	event LogWithdrawal(address indexed _receiver, uint _value);
 
 	mapping(address => uint) balances;
 
 	// modifier is alice
 
 	function Splitter(address _bob, address _carol) public {
+		alice = msg.sender;
 		bob = _bob;
 		carol = _carol;
-		alice = msg.sender;
 	}
 
 	function getBalance() public view returns (uint) {
 		return this.balance;
 	}
 	
-	function deposit() public payable {
+	function split() public payable {
 	    require(msg.sender == alice);
-	    balances[alice] += msg.value;
-	    logDeposit(msg.value);
-	}
-
-	function split() public returns (bool) {
-		require(msg.sender == alice);
-		uint half = balances[alice] / 2;
-	    balances[alice] = 0;
-		balances[carol] += half;
-		balances[bob] += half;
-		logSplit(half * 2);
-		return (true);
+	    uint half = msg.value/2;
+	    balances[bob] += half;
+	    balances[carol] += half;
+	    LogSplit(balances[bob], balances[carol]);
 	}
 	
-	function withdraw() public returns(address) {
+	function withdraw() public {
 	    uint value = balances[msg.sender];
 	    balances[msg.sender] = 0;
 	    msg.sender.transfer(value);
-	    return msg.sender;
-	    logWithdrawal(msg.sender, value);
+	    LogWithdrawal(msg.sender, value);
 	}
 
 	function getIndividualBalance() public view returns (
 		uint, address,
 		uint, address,
-		uint, address) {
-
-		return(balances[alice], alice, balances[bob], bob, balances[carol], carol);
-		    
+		uint, address
+		) {
+		return(
+		    balances[alice], alice, 
+		    balances[bob], bob, 
+		    balances[carol], carol
+		    );
 		}
 		
 	function() public {
-}
+    }
 }
