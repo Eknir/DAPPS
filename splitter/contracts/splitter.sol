063e0cfb4b69953de7464a9024af4f7d880fe6c8
Rinke Hendriksen <33000441+Eknir@users.noreply.github.com>
2018-03-06 13:20:11 +0100

    Add files via upload

--- splitter/contracts/splitter.sol
+++ splitter/contracts/splitter.sol
@@ -1,29 +1,59 @@
 pragma solidity ^0.4.17;
 
 contract Splitter {
-	address Alice;
-	address Bob = 0x627306090abab3a6e1400e9345bc60c78a8bef57; // some random ethereum address
-	address Carol = 0xf17f52151ebef6c7334fad080c5704d77216b732; // some random ethereum address
+	address alice;
+	address bob = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c; // fill in yourself!
+	address carol = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db; // fill in yourself!
+	
+	event logDeposit(uint _depositAmount);
+	event logWithdrawal(address _receiver, uint _value);
+	event logSplit(uint _splitAmount);
 
-	function Splitter() public payable {
-		Alice = msg.sender;
+	mapping(address => uint) balances;
+
+	// modifier is alice
+
+	function Splitter() public {
+		alice = msg.sender;
 	}
 
 	function getBalance() public view returns (uint) {
 		return this.balance;
 	}
+	
+	function deposit() public payable {
+	    require(msg.sender == alice);
+	    balances[alice] += msg.value;
+	    logDeposit(msg.value);
+	}
 
-	function splitAlice() public payable returns (bool) {
-		assert(msg.sender == Alice)
-		//TODO
+	function split() public returns (bool) {
+		require(msg.sender == alice);
+		uint half = balances[alice] / 2;
+	    balances[alice] = 0;
+		balances[carol] += half;
+		balances[bob] += half;
+		logSplit(half * 2);
+		return (true);
+	}
+	
+	function withdraw() public returns(address) {
+	    uint value = balances[msg.sender];
+	    balances[msg.sender] = 0;
+	    msg.sender.transfer(value);
+	    return msg.sender;
+	    logWithdrawal(msg.sender, value);
 	}
 
 	function getIndividualBalance() public view returns (
-		uint,
-		uint,
-		uint)
+		uint, address,
+		uint, address,
+		uint, address) {
 
-	function() payable {
+		return(balances[alice], alice, balances[bob], bob, balances[carol], carol);
+		    
+		}
 		
-	}
+	function() public {
+}
 }
