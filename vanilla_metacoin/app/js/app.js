cf7cd5a194761dbbbf6b51948e92c987c6b22d90
Rinke Hendriksen <33000441+Eknir@users.noreply.github.com>
2018-03-13 14:41:09 +0100

    Add files via upload
    Copy/paste the app/js/app.js from B9 works. Did I make a typo?

--- vanilla_metacoin/app/js/app.js
+++ vanilla_metacoin/app/js/app.js
@@ -41,59 +41,72 @@ window.addEventListener('load', function() {
 	})
 	.then(deployed => deployed.getBalance.call(window.account))
 	.then(balance => $("#balance").html(balance.toString(10)))
-	.then(() => $("#send").click(sendCoin))
-	.catch(console.error);
+    // We wire it when the system looks in order.
+    .then(() => $("#send").click(sendCoin))
+    // Never let an error go unlogged.
+    .catch(console.error);
 });
 
 const sendCoin = function() {
-	const gas = 300000; let deployed;
-	// return the whole promise chain so that parts of the UI can be informed when done
-	return MetaCoin.deployed()
-	// simulate the real call
-		.then(_deployed => {
-			return _deployed.sendCoin.call(
-				$("input[name='recipient']").val(),
-				$("input[name='amount']").val(),
-				{ from: window.account, gas: gas });
-		})
-		.then(success => {
-			if (!success) {
-				throw new Error("The transaction will fail anyway, not sending");
-			}
-			// move on to proper action
-			//.sendTransaction so we get the txHash immediately 
-			return deployed.sendCoin.sendTransaction(
-				$("input[name='recipient']").val(),
-				$("input[name='amount']").val(),
-				{ from: window.account, gas: gas });
-		})
-		.then(txHash => {
-			$("#status").html("Transaction on the way " + txHash);
-			const tryAgain = () => web3.eth.getTransactionReceipt(txHash)
-				.then(receipt => receipt !== null ?
-					receipt :
-					Promise.delay(1000).then(tryAgain));
-				return tryAgain();
-		})
-		.then(receipt => {
-			if (receipt.logs.length == 0) {
-				console.error("Empty logs");
-				console.error(receipt);
-				$("#status").html("There was an error in the tx execution");
-			} else {
-			// format the event nicely
-				console.log(receipt.logs[0].args);
-				$("#status").html("Transfer executed");
-			}
-			//update UI
-			return deployed.getBalance.call(window.account)
-		})
-		.then(balance => $("#balance").html(balance.toString(10)))
-		.catch(e => {
-			$("#status").html(e.toString());
-			console.error(e);
-		});
+    // Sometimes you have to force the gas amount to a value you know is enough because
+    // `web3.eth.estimateGas` may get it wrong.
+    const gas = 300000; let deployed;
+    // We return the whole promise chain so that other parts of the UI can be informed when
+    // it is done.
+    return MetaCoin.deployed()
+        .then(_deployed => {
+            deployed = _deployed;
+            // We simulate the real call and see whether this is likely to work.
+            // No point in wasting gas if we have a likely failure.
+            return _deployed.sendCoin.call(
+                $("input[name='recipient']").val(),
+                // Giving a string is fine
+                $("input[name='amount']").val(),
+                { from: window.account, gas: gas });
+        })
+        .then(success => {
+            if (!success) {
+                throw new Error("The transaction will fail anyway, not sending");
+            }
+            // Ok, we move onto the proper action.
+            // .sendTransaction so that we get the txHash immediately while it 
+            // is mined, as, in real life scenarios, that can take time of 
+            // course.
+            return deployed.sendCoin.sendTransaction(
+                $("input[name='recipient']").val(),
+                // Giving a string is fine
+                $("input[name='amount']").val(),
+                { from: window.account, gas: gas });
+        })
+        // We would be a proper `txObject` if we had not added `.sendTransaction` above.
+        // Oh well, more work is needed here for our user to have a nice UI.
+        .then(txHash => {
+            $("#status").html("Transaction on the way " + txHash);
+            // Now we wait for the tx to be mined. Typically, you would take this into another
+            // module, as in https://gist.github.com/xavierlepretre/88682e871f4ad07be4534ae560692ee6
+            const tryAgain = () => web3.eth.getTransactionReceiptPromise(txHash)
+                .then(receipt => receipt !== null ?
+                    receipt :
+                    // Let's hope we don't hit the max call stack depth
+                    Promise.delay(1000).then(tryAgain));
+            return tryAgain();
+        })
+        .then(receipt => {
+            if (receipt.logs.length == 0) {
+                console.error("Empty logs");
+                console.error(receipt);
+                $("#status").html("There was an error in the tx execution");
+            } else {
+                // Format the event nicely.
+                console.log(deployed.Transfer().formatter(receipt.logs[0]).args);
+                $("#status").html("Transfer executed");
+            }
+            // Make sure we update the UI.
+            return deployed.getBalance.call(window.account);
+        })
+        .then(balance => $("#balance").html(balance.toString(10)))
+        .catch(e => {
+            $("#status").html(e.toString());
+            console.error(e);
+        });
 };
-
-console.log(MetaCoin);
-console.log(MetaCoin.deployed())
